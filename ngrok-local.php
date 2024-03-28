9cac7d9c0e48613e71d143fd815a82d14e79e59f
Oskar Modig <accounts@oskarmodig.com>
2024-03-28 13:57:38 +0100

    Refactor Ngrok_Local class and improve SSL support
    Refactored the Ngrok_Local class for better readability and maintainability by adding docblocks and fixing syntax inconsistencies. Enhanced SSL support by considering the site's protocol (HTTP/HTTPS) dynamically when setting WP_SITEURL and WP_HOME constants. Fixed redirection logic to better handle the 'wp_ngrok_autoload' query parameter and improve ngrok_url assignment.

--- ngrok-local.php
+++ ngrok-local.php
@@ -9,35 +9,78 @@
  * License: GPLv2+
  */
 
+/**
+ * Class Ngrok_Local
+ */
 class Ngrok_Local {
 
-	private $site_url;
+	/**
+	 * The local site url.
+	 *
+	 * @var string
+	 */
+	private string $site_url;
 
-	public function __construct(){
-		$this->site_url = site_url() . '/';;
+	/**
+	 * Store the site url and set the constants.
+	 */
+	public function __construct() {
+		$this->site_url = site_url() . '/';
 
-		if ( ! defined( 'WP_SITEURL' ) && ! defined( 'WP_HOME' ) && isset( $_SERVER['HTTP_HOST'] ) ) {
-			define( 'WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] );
-			define( 'WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] );
+		if (
+			! defined( 'WP_SITEURL' ) &&
+			! defined( 'WP_HOME' ) &&
+			isset( $_SERVER['HTTP_HOST'] )
+		) {
+			$protocol = is_ssl() ? 'https://' : 'http://';
+
+			define( 'WP_SITEURL', $protocol . $_SERVER['HTTP_HOST'] );
+			define( 'WP_HOME', $protocol . $_SERVER['HTTP_HOST'] );
 		} else {
-			// bail if those constant are already defined
-			return false;
+			// Bail if those constants are already defined.
+			return;
 		}
 
 		add_action( 'template_redirect', array( $this, 'template_redirect' ) );
 	}
 
-	public function template_redirect() {
+	/**
+	 * Replace the site url with the ngrok url in content.
+	 *
+	 * @return void
+	 */
+	public function template_redirect(): void {
+		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		if ( ! isset( $_GET['wp_ngrok_autoload'] ) ) {
 			$protocol = is_ssl() ? 'https://' : 'http://';
+
+			$request  = wp_remote_get(
+				add_query_arg(
+					'wp_ngrok_autoload',
+					1,
+					$protocol . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']
+				)
+			);
+			$response = wp_remote_retrieve_body( $request );
+
+			if ( defined( 'WP_LOCAL_NGROK_URL' ) ) {
+				$ngrok_url = WP_LOCAL_NGROK_URL;
+			} elseif ( ! empty( $_SERVER['HTTP_REFERER'] ) ) {
+				$parsed_ngrok_url = wp_parse_url( $_SERVER['HTTP_REFERER'] );
+				$ngrok_url        = $parsed_ngrok_url['scheme'] . '://' . $parsed_ngrok_url['host'];
+			} else {
+				$ngrok_url = wp_make_link_relative( $this->site_url );
+			}
+
+			// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 			echo str_replace(
 				$this->site_url,
-				wp_make_link_relative( $this->site_url ),
-				file_get_contents( add_query_arg( 'wp_ngrok_autoload', 1, $protocol . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] ) )
+				$ngrok_url,
+				$response
 			);
 			exit;
 		}
 	}
 }
 
-new Ngrok_Local;
+new Ngrok_Local();
